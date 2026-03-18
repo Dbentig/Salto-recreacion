c0c89e69c612053f2babfff42e988eb93b8bf10e
Dbentig <dbentig@gmail.com>
2026-03-18 22:12:50 +0100

    Publicación

--- Scripts/camara_3d.gd
+++ Scripts/camara_3d.gd
@@ -1,10 +1,10 @@
 extends Node3D
 
-@export var objetivo: Node3D  # arrastra aquí tu Jugador
+@export var objetivo: Node3D 
 @export var offset: Vector3 = Vector3(0, 8, 1.2)
 @export var suavizado: float = 0.15
 
-func _physics_process(delta):
+func _physics_process(_delta: float):
 	if objetivo != null:
 		var target_pos = objetivo.global_transform.origin + offset
 		global_transform.origin = global_transform.origin.lerp(target_pos, suavizado)
--- Scripts/cubito.gd
+++ Scripts/cubito.gd
@@ -6,28 +6,53 @@ const JUMP_VELOCITY = 4.5
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# gravedad.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y -= 7 * 10 * delta
+	else:
+		velocity.y = 0
+		#Salto (juego)
+		if Input.get_action_strength("salto"):
+			velocity.y = 30
+	 
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		
+
+	# inputs de movimiento.
 
 	var direction := Vector3.ZERO
 	var input_x = Input.get_action_strength("derecha") - Input.get_action_strength("izquierda")
 	var input_z = Input.get_action_strength("abajo") - Input.get_action_strength("arriba")
 	direction.x = input_x
 	direction.z = input_z
+	var dash = true
+	var dash_speed = 40
+	var dash_cooldown = 2.0
+	var dash_duration = 0.2
+	var dashing = false	
 
-	if direction:
+
+	if direction.length() > 0:
+		#rotacion
+		direction = direction.normalized()
+		var objetivo := atan2(direction.x, direction.z)
+		rotation.y = lerp_angle(rotation.y, objetivo, 0.1)
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-	if direction.length() > 0:
-		direction = direction.normalized()
-		var objetivo := atan2(direction.x, direction.z)
-		rotation.y = lerp_angle(rotation.y, objetivo, 0.1)
+	if Input.is_action_just_pressed("dash") and dash:
+		dash = false
+		dashing = true
+		velocity = direction * dash_speed
+		await get_tree().create_timer(dash_duration).timeout
+		dashing = false
+		await get_tree().create_timer(dash_cooldown).timeout
+		dash = true
+		
+		
+		
+		
 
 	move_and_slide()
