f79a19505d7655cd13a6332633afbef13d538888
Dbentig <dbentig@gmail.com>
2026-03-23 23:18:30 +0100

    Implementacion del dash y keyblind

--- Scripts/camara_3d.gd
+++ Scripts/camara_3d.gd
@@ -1,10 +1,21 @@
 extends Node3D
 
 @export var objetivo: Node3D 
-@export var offset: Vector3 = Vector3(0, 8, 1.2)
+var distance = Vector3(0.0, 8.0, 1.2)
+var distance_multipler = 1
 @export var suavizado: float = 0.15
 
 func _physics_process(_delta: float):
+	var offset: Vector3 = Vector3(distance * distance_multipler)
+	if Input.get_action_strength("camara+"):
+		distance_multipler += 0.05
+	if Input.get_action_strength("camara-"):
+		distance_multipler -= 0.05
 	if objetivo != null:
 		var target_pos = objetivo.global_transform.origin + offset
 		global_transform.origin = global_transform.origin.lerp(target_pos, suavizado)
+	if Input.get_action_strength("camara+"):
+		distance_multipler += 0.02
+	if Input.get_action_strength("camara-"):
+		distance_multipler -= 0.02
+	
--- Scripts/cubito.gd
+++ Scripts/cubito.gd
@@ -3,7 +3,12 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-
+var dash = true
+var dash_speed = 40
+var dash_cooldown = 2.0
+var dash_duration = 0.2
+var dashing = false	
+var dash_velocity = Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
 	# gravedad.
@@ -14,22 +19,17 @@ func _physics_process(delta: float) -> void:
 		#Salto (juego)
 		if Input.get_action_strength("salto"):
 			velocity.y = 30
-	 
-
-		
-
 	# inputs de movimiento.
 
 	var direction := Vector3.ZERO
+	
 	var input_x = Input.get_action_strength("derecha") - Input.get_action_strength("izquierda")
 	var input_z = Input.get_action_strength("abajo") - Input.get_action_strength("arriba")
 	direction.x = input_x
 	direction.z = input_z
-	var dash = true
-	var dash_speed = 40
-	var dash_cooldown = 2.0
-	var dash_duration = 0.2
-	var dashing = false	
+	#input del dash
+	if Input.is_action_just_pressed("dash") and dash:
+			funcion_dash(direction)
 
 
 	if direction.length() > 0:
@@ -37,22 +37,36 @@ func _physics_process(delta: float) -> void:
 		direction = direction.normalized()
 		var objetivo := atan2(direction.x, direction.z)
 		rotation.y = lerp_angle(rotation.y, objetivo, 0.1)
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		
+		#movimiento normal
+		if not dashing:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = dash_velocity.x
+			velocity.z = dash_velocity.z
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-	if Input.is_action_just_pressed("dash") and dash:
-		dash = false
-		dashing = true
-		velocity = direction * dash_speed
-		await get_tree().create_timer(dash_duration).timeout
-		dashing = false
-		await get_tree().create_timer(dash_cooldown).timeout
-		dash = true
-		
-		
-		
-		
-
+	move_and_slide()
+#dash
+func funcion_dash(direction):
+	dash = false
+	dashing = true
+	direction = direction.normalized()
+	#ver la direccion de Cubito
+	var apuntando = transform.basis.z
+	apuntando.y = 0
+	apuntando = apuntando.normalized()
+	#movimiento del dash
+	if direction.length() > 0:
+		dash_velocity = direction * dash_speed
+	else:
+		dash_velocity = apuntando * dash_speed * 2
+	velocity.x = dash_velocity.x
+	velocity.z = dash_velocity.z
+	await get_tree().create_timer(dash_duration).timeout
+	dashing = false
+	await get_tree().create_timer(dash_cooldown).timeout
+	dash = true
 	move_and_slide()
